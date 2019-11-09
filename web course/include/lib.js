5f154e4a4863f92105e938c4ca0cd98fb11e6803
Daniel Tibi <cky800@gmail.com>
2019-11-09 20:30:23 +0200

    added functionality, jquery,bootswatch

---
+++ web course/include/lib.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $(".btn").on("click", function () {
+        var t = $(this).text();
+        var o = $("#screen").text();
+        if (t == "=") {
+            var str = eval(o);
+            var res = o + "=" + str;
+            $("#screen").text(res);
+        } else if (t == "מחק") {
+            var s = o.substr(0, o.length - 1);
+            $("#screen").text(s);
+        } else if (t == "מחק הכל") {
+            $("#screen").text("");
+        } else {
+            var s = $("#screen").text();
+            s += t;
+            $("#screen").text(s);
+        }
+    })
+})
