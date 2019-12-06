a9732593807a749c38205c43158a961a9b0d0a1f
Daniel Tibi <cky800@gmail.com>
2019-12-06 21:57:38 +0200

    added lab6 wip

---
+++ web course/Part6/lab/inc/app.js
@@ -0,0 +1,29 @@
+﻿angular.module('sortApp', [])
+
+.controller('mainController', function ($scope) {
+    $scope.sortType = 'name'; // set the default sort type
+    $scope.sortReverse = false;  // set the default sort order
+    $scope.searchFish = '';     // set the default search/filter term
+
+    // create the list of sushi rolls 
+    $scope.sushi = [
+      { name: 'Cali Roll', fish: 'Crab', tastiness: 2 },
+      { name: 'Philly', fish: 'Tuna', tastiness: 4 },
+      { name: 'Tiger', fish: 'Eel', tastiness: 7 },
+      { name: 'Rainbow', fish: 'Variety', tastiness: 6 }
+    ];
+    // backup for $scope.sushi
+    $scope.sushiBac = angular.copy($scope.sushi);
+
+    $scope.save = function () {
+        $scope.sushiBac = angular.copy($scope.sushi);
+        $("tbody td").css("color", "black");
+    }
+
+    $scope.reset = function () {
+        $scope.sushi = angular.copy($scope.sushiBac);
+        $("tbody td").css("color", "black");
+    }
+
+    
+});
