3d58f567f923b304eba0829a7833a0c0f29b6cd9
Daniel Tibi <cky800@gmail.com>
2019-12-18 13:41:06 +0200

    working on add new recipe form. (Save)

--- finalProject/include/app.js
+++ finalProject/include/app.js
@@ -11,9 +11,10 @@ DAIRY = 2;
 MEAT = 4;
 FISH = 8;
 
-angular.module('kitchenApp', [])
 
-    .controller('mainController', function ($scope) {
+// new recipe form definitions.
+index = 0;
+angular.module('kitchenApp', []).controller('mainController', function ($scope) {
     var rec_path = "https://www.diabetes.org/sites/default/files/styles/crop_large/public/2019-06/Healthy%20Food%20Made%20Easy%20-min.jpg";
     var ing_path = "https://image.shutterstock.com/image-photo/red-apple-on-white-background-600w-158989157.jpg";
     var ing_1 = new Ingredient(0, "Banana", "Pc", ing_path, 0);
@@ -25,4 +26,22 @@ angular.module('kitchenApp', [])
     }
     console.log("done with assignment")
     console.log($scope.recipes)
+
 });
+
+
+function new_raw() {
+    console.log("new line clicked!");
+    markup = "<tr> \
+                <th style='width:30px' id='index" + index + "'>" + index + "</th>\
+                <th style='width:120px'><input id='name" + index + "' \></th>\
+                <th style='width:60px'><input type='integer' id='age" + index + "' \></th>\
+                <th><input id='address" + index + "' \></th>\
+                <th><button class='btn btn-danger' onclick='del_raw("+ index + ")'>x</button></th>\
+            </tr>";
+    index++;
+    $("#tbIngrediants").append(markup);
+}
+function del_raw(i) {
+    $("#index" + i).parent().remove();
+}
